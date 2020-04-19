ba56e3e98929d2af7924b57a9e75c3d3296ed262
Suwadith <suwadith.srithar@gmail.com>
2020-04-20 04:24:39 +0530

    started to work on the web-app

--- Web_App/app.py
+++ Web_App/app.py
@@ -1,17 +1,32 @@
-from flask import Flask
-from flask import render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import numpy as np
 import traceback
 import pickle
 import pandas as pd
+import re
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
+combinedLeagueDataframe = pd.read_csv("../Pre_Processed_Datasets/League-Team.csv")
+combinedPlayerDataframe = pd.read_csv("../Pre_Processed_Datasets/Preprocessed Player Data.csv")
+forecastedPlayerDataframe = pd.read_csv("../Pre_Processed_Datasets/Forecasted Ratings 2019-2022.csv")
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    leagues = load_leagueNames()
+    return render_template('index.html', leagues=leagues)
 
 
 if __name__ == '__main__':
     app.run()
+    # app.run(debug=True)
+
+#
+# def load_datasets():
+
+def load_leagueNames():
+    leagues = combinedLeagueDataframe.League.unique().tolist()
+    leagueDict = {}
+    for i in leagues:
+        leagueDict[i] = re.sub(r"(\w)([A-Z])", r"\1 \2", i)
+    return leagueDict
